589cf4e523f41a7089235ce5dc0a585ef722bda2
Yousef <yousefnamazi@hotmail.com>
2019-04-23 12:40:54 +0200

    Hello world

---
+++ src/sample/Database.java
@@ -0,0 +1,32 @@
+package sample;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+    private static Connection connection = null;
+    private static String url = "";
+    private static String user = "";
+    private static String pass = "";
+
+    // Establish a new connection without needing to create new connections if it does already exist.
+    private Connection getCurrentConnection() {
+        if (connection != null) {
+            return connection;
+        } else {
+            try {
+                connection = DriverManager.getConnection(url,user,pass);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return connection;
+    }
+
+    private Product returnProduct(int id, String name, int quantity) {
+        return null;
+    }
+
+}
