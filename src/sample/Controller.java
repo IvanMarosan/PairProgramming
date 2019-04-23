fb9ce23807fdbfb55a18c46d85de8a6033e159a3
JimboMan123 <tauriainenjimmie@live.se>
2019-04-23 12:57:32 +0200

    Merge branch 'master' of https://github.com/IvanMarosan/PairProgramming
    # Conflicts:
    #	.idea/misc.xml

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,18 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
 public class Controller {
+
+    @FXML
+    private ListView<?> listView;
+
+    @FXML
+    void buyItems(ActionEvent event) {
+
+
+    }
 
 }
